6f35618570152842ade537ad7c3372dc0245549d
Bruno Máximo <Bmaximo93@gmail.com>
2025-01-25 16:51:47 -0300

    teste

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,104 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public UnityEngine.AI.NavMeshAgent agent;
+    private GameObject player;
+    private NavMeshAgent agent;
 
-    public Transform player;
+    //public LayerMask whatIsGround, whatIsPlayer;
 
-    public LayerMask whatIsGround, whatIsPlayer;
+    private float health = 5;
 
-    public float health;
+    private bool canMove = true;
 
-    //patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    public float attackInterval;
-    bool hasAttacked;
+    //public float attackInterval;
+    //bool hasAttacked;
 
     //states
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
+    //public float sightRange, attackRange;
+    //public bool playerInSightRange, playerInAttackRange;
 
     private void Awake()
     {
-        player = GameObject.Find("PlayerObj").transform;
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-    }
+        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
 
-    private void Patrol()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-        if (walkPointSet) agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
-    }
-
-    private void SearchWalkPoint()
-    {
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-        walkPointSet = true;
-    }
-
-    private void ChasePlayer()
-    {
-        agent.SetDestination(player.position);
-    }
-
-    private void AttackPlayer()
-    {
-        agent.SetDestination(transform.position);
-        transform.LookAt(player);
-
-        if(!hasAttacked)
-        {
-            //logica de ataque
-            hasAttacked = true;
-            Invoke(nameof(ResetAttack), attackInterval);
-        }
-    }
-
-    private void ResetAttack()
-    {
-        hasAttacked = false;
-    }
-
-    void Start()
-    {
-        
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        playerInSightRange = Physics.ChechSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.ChechSphere(transform.position, attackRange, whatIsPlayer);
-
-        if (!playerInSightRange && !playerInAttackRange) Patrol();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        DeathLogic();
     }
 
-    public void TakeDamage(int damage)
+    public void FixedUpdate()
     {
-        health -= damage;
-
-        if(health <= 0) invoke(nameof(DestroyEnemy), .5f);
-        
+        MoveLogic();
     }
 
-    private void DestroyEnemy()
+    public void MoveLogic()
     {
-        Destroy(GameObject);
+        if (canMove)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        else
+            agent.velocity = Vector3.zero;
+    }
+
+    public void DeathLogic()
+    {
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            health -= 1;
+            other.gameObject.SetActive(false);
+        }
     }
 }
